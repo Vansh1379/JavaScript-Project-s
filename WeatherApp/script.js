5fad15f50c8068a4bc5c87f4c02877aa77181581
vansh kalra <vanshkalra1379@gmail.com>
2024-02-11 11:23:43 +0530

    Todo app

---
+++ WeatherApp/script.js
@@ -0,0 +1,11 @@
+
+    const apikey = "c8e6c18f5cc36f5bafbd642a4123728b";
+    const apiurl = "https://api.openweathermap.org/geo/1.0/direct?&units=metric&q=karnal";
+
+    async function checkWheather(){
+        const response = await fetch(apiurl+`&appid=${apiKey}`+`&limit=5`);
+        var data = await response.json();
+
+        console.log(data);
+    }
+    checkWheather();
