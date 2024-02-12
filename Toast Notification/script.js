6b9d29df8bb8ba7be21f9796d95b251debcd96d8
vansh kalra <vanshkalra1379@gmail.com>
2024-02-12 18:16:58 +0530

    Toast Notification

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -1,8 +1,23 @@
 
-    let toastBox = document.getElementById("toastBox");
-    function showToast(){
+    let toastBox = document.getElementById('toastBox');
+    let SuccessMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Submitted';
+    let ErrorMsg = '<i class="fa-solid fa-circle-xmark"></i> Dumb Fix The Error First !';
+    let WarningMsg = '<i class="fa-solid fa-circle-exclamation"></i> Warning Bro Invalid Input';
+
+    function showToast(msg){
        let Toast = document.createElement('div');
        Toast.classList.add("toast");
-       Toast.innerHTML = "Success";
-       toastBox.appendChild("Toast");
+       Toast.innerHTML = msg;
+       toastBox.appendChild(Toast);
+
+       if(msg.includes('Error')){
+        Toast.classList.add('error');
+       }
+       if(msg.includes('Warning')){
+        Toast.classList.add("Warning");
+       }
+
+       setTimeout(()=>{
+        Toast.remove();
+       },6000)
     }
