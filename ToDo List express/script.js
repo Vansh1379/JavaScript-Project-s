7a7fa42d78d7ce805c99e0df1b37708383ffcec1
vansh kalra <vanshkalra1379@gmail.com>
2024-02-26 00:25:16 +0530

    Express

--- ToDo List express/script.js
+++ ToDo List express/script.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res)=>{
-    res.send("Hello World");
+app.get('/',(req,res)=>{
+    res.send('<b>Hi cutiee!</b>');
 })
+// app.post("/backend", (req,res)=>{ 
+//     res.send('<b>Hi There!</b>');
+// })
 
 app.listen(port, ()=>{
     console.log(`server is running in  http://localhost:${port}`);
